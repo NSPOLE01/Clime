a1224b094390b82b1cb48009cc886f56388a7739
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-01 17:29:47 -0400

    add in struct for better readability

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -8,23 +8,23 @@
 
 import Foundation
 
-struct WeatherManager{
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=3c3f45fe2d238b6128c400385c971e52&units=metric"
-    
-    func fetchWeather(cityName: String){
+struct WeatherManager {
+    let weatherURL =
+        "https://api.openweathermap.org/data/2.5/weather?units=metric"
+
+    func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         performRequest(urlString: urlString)
     }
-    
-    func performRequest(urlString: String){
-        if let url = URL(string: urlString){
+
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
                     print(error!)
                     return
                 }
-                
                 if let safeData = data {
                     self.parseJSON(weatherData: safeData)
                 }
@@ -33,14 +33,23 @@ struct WeatherManager{
 
         }
     }
-    
-    func parseJSON(weatherData: Data){
+
+    func parseJSON(weatherData: Data) {
         let decoder = JSONDecoder()
-        do{
-            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
+        do {
+            let decodedData = try decoder.decode(
+                WeatherData.self,
+                from: weatherData
+            )
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
     }
+
 }
