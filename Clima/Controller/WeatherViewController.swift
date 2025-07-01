961d92a46fc580647c8fc48785b222872f637343
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-01 16:43:58 -0400

    initial api call

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchField.delegate = self
@@ -31,7 +33,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
+        if let city = searchField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
         searchField.text = ""
     }
     
---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Nikhil Polepalli on 7/1/25.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager{
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            task.resume()
+            
+            
+                
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        
+    }
+}
