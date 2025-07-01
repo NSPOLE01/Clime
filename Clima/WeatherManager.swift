55b3ac457f921768b2b6dbd340b0c8c4e4089a8c
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-01 17:02:08 -0400

    use closure

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManager{
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -19,25 +19,22 @@ struct WeatherManager{
     func performRequest(urlString: String){
         if let url = URL(string: urlString){
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
             task.resume()
             
             
                 
             
         }
-    }
-    
-    func handle(data: Data?, response: URLResponse?, error: Error?){
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
-        
     }
 }
